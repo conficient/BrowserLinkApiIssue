1a16bc25026c7ca4dfbe57d71fb48e153bbe0b40
HowardRichards <dev@conficient.com>
2013-12-03 14:53:55 +0000

    Inital commit showing BrowserLink problem

---
+++ WebApplication1/WebApplication1/Api/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Api
+{
+    public class TestController : ApiController
+    {
+        // GET api/<controller>
+        public TestModel GetObject()
+        {
+            return new TestModel();
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Api/TestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Api
+{
+    /// <summary>
+    /// Dummy class that returns a comment containing html
+    /// </summary>
+    public class TestModel
+    {
+        public TestModel()
+        {
+            this.Name = "Some user";
+            this.Comment = "<html><head></head><body><p>This is a comment</p></body></html>";
+        }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+    }
+}
